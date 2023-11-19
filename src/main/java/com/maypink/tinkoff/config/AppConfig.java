e93971a9741fe87811156a80ca6b7b46338cd438
Pinchuk Maya <mayaprr@yandex.ru>
2023-11-19 10:18:49 +0300

    initial setup

--- src/main/java/com/maypink/tinkoff/config/AppConfig.java
+++ src/main/java/com/maypink/tinkoff/config/AppConfig.java
@@ -10,6 +10,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
 
 @OpenAPIDefinition(
